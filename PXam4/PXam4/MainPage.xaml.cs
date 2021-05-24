3c9302e5ba32582485f6b344e3de8fa73ebd0558
Alexis SOUSSAN <asoussan@lerebours.org>
2021-05-24 13:53:56 +0200

    Ajoutez des fichiers projet.

---
+++ PXam4/PXam4/MainPage.xaml.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+
+
+namespace PXam4
+{
+    public partial class MainPage : ContentPage
+    {
+        Model1 gst:
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ Xam4Classe/GstBdd.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MySql.Data.MySqlClient;
+
+namespace Xam4Classe
+{
+    public class GstBdd
+    {
+        // cnx sert à se connecter à la BDD
+        private MySqlConnection cnx;
+        // cmd sert à écrire nos requêtes
+        private MySqlCommand cmd;
+        // dr me permet de récupérer le jeu d'enregistrement(s) de ma requête (select)
+        private MySqlDataReader dr;
+
+        // Constructeur
+        public GstBdd()
+        {
+            // La chaine va nous permettre de donner
+            // 1) le nom du serveur
+            // 2) le nom de la base de données
+            // 3) le nom de l'utilisateur
+            // 4) le mot de passe
+            string chaine = "Server=localhost;Database=xambibliotheque;Uid=root;Pwd=";
+            cnx = new MySqlConnection(chaine);
+            cnx.Open();
+        }
+    }
+}
